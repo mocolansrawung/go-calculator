ebf0a7bee779e73671e5e67ad2d91c07063f2cf1
Yudho Ahmad Fahreza <yudhoahmadz@gmail.com>
2023-07-26 13:33:43 +0700

    add function call and fix variable names

--- internal/model/calculator.go
+++ internal/model/calculator.go
@@ -3,26 +3,26 @@ package model
 import "fmt"
 
 type Calculator struct {
-	NumberOne int
-	NumberTwo int
+	firstNumber  int
+	secondNumber int
 }
 
-func NewCalculator(numOne, numTwo int) *Calculator {
-	return &Calculator{NumberOne: numOne, NumberTwo: numTwo}
+func NewCalculator(firstNum, secondNum int) *Calculator {
+	return &Calculator{firstNumber: firstNum, secondNumber: secondNum}
 }
 
 func (c *Calculator) Subtract() int {
-	return c.NumberOne - c.NumberTwo
+	return c.firstNumber - c.secondNumber
 }
 func (c *Calculator) Addition() int {
-	return c.NumberOne + c.NumberTwo
+	return c.firstNumber + c.secondNumber
 }
 
 func (c *Calculator) Multiply() int {
-	return c.NumberOne + c.NumberTwo
+	return c.firstNumber + c.secondNumber
 }
 
 func (c *Calculator) PrintInfo() {
-	fmt.Printf("number one: %v\n", c.NumberOne)
-	fmt.Printf("number two: %v\n", c.NumberTwo)
+	fmt.Printf("First Number: %v\n", c.firstNumber)
+	fmt.Printf("Second Number: %v\n", c.secondNumber)
 }
