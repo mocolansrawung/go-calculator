666a9857b19ec61e9d7317c3051b68fa8c921492
Muhammad Nuriansyah Malik Nugraha <nuriansyah09@gmail.com>
2023-07-26 13:49:01 +0700

    fix: "Add"

--- internal/model/calculator.go
+++ internal/model/calculator.go
@@ -18,20 +18,19 @@ func (c *Calculator) Subtract() int {
 }
 
 func (c *Calculator) Add() int {
-	return c.NumberOne + c.NumberTwo
-
+	return c.firstNumber + c.secondNumber
+}
 func (c *Calculator) Divide() float64 {
 	result := float64(c.NumberOne) / float64(c.NumberTwo)
 	formattedResult := fmt.Sprintf("%.2f", result)
 
 	var roundedResult float64
-    _, err := fmt.Sscanf(formattedResult, "%f", &roundedResult)
-    if err != nil {
-        fmt.Println(err)
-    }
-	return roundedResult  
+	_, err := fmt.Sscanf(formattedResult, "%f", &roundedResult)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return roundedResult
 }
-
 
 func (c *Calculator) Multiply() int {
 	return c.firstNumber + c.secondNumber
