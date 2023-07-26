4a420ce784f9b3118257a56b5a0f3988a4b63873
tamiraminh <140486349+tamiraminh@users.noreply.github.com>
2023-07-26 13:58:29 +0700

    hot fix divide function (#9)

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func main() {
 	calc := model.NewCalculator(1, 3)
 
 	fmt.Println(calc.Subtract())
-  fmt.Println(calc.Addition())
+  	fmt.Println(calc.Add())
 	fmt.Println(calc.Divide())
 	fmt.Println(calc.Multiply())
 
